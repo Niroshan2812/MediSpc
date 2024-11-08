7f5c3e11d6e3f617d414a76dc009d864a4f37b65
Niroshan <niroshan9812@gmail.com>
2024-11-09 01:41:22 +0530

    HomeScreen List Rendering include

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,17 @@
-import {View, Text} from 'react-native'
-export default function HomeScreen(){
-    return(
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+import { View, Text, FlatList } from 'react-native'
+import { meditations } from '@/data';
+import {MeditationListItem} from '@/components/MeditationListItem';
+
+
+export default function HomeScreen() {
+    return (
+        <>
+           <FlatList 
+           data = {meditations}
+           className='bg-white'
+           contentContainerClassName='gap-5 p-3'
+           renderItem={({item}) => <MeditationListItem meditation={item} />} />
+        </>
+
     )
 }
